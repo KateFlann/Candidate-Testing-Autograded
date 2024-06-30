7cbf204c100b2be2e094bb012f4641ad57f76fca
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-06-30 09:58:00 -0400

    Final Submission for Assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,8 +1,8 @@
-const input = require('readline-sync');
+const input = require("readline-sync");
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
+// TODO 1.1a: Define candidateName //
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
@@ -10,29 +10,21 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
-
 //TODO: Variables for Part 2
 let questions = [
   "Who was the first American woman in space? ",
-  "True or false: 5 kilometer == 5000 meters? ", 
+  "True or false: 5 kilometer == 5000 meters? ",
   "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-  "What is the minimum crew size for the ISS? "
+  "What is the minimum crew size for the ISS? ",
 ];
 
-let correctAnswers = [
-  "Sally Ride",
-  "true",
-  "40",
-  "Trajectory",
-  "3"
-];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name ///
-   candidateName = input.question("What is your name?");
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
@@ -40,36 +32,49 @@ function askQuestion() {
   for (let i = 0; i < questions.length; i++) {
     let a = input.question(questions[i]);
     candidateAnswers.push(a);
- }
-
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+  let grade = [];
   for (let i = 0; i < correctAnswers.length; i++) {
-  
-    if (candidateAnswers[i] === correctAnswers[i]) {
-      console.log(`Correct! The answer is ${correctAnswers[i]}`)
+    if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()) {
+      grade.push(correctAnswers[i]);
+      console.log(`Correct! The answer is ${correctAnswers[i]}`);
     } else {
-      console.log(`Incorrect. You answered ${candidateAnswers[i]}, but the correct answer is ${correctAnswers[i]}.`)
+      console.log(
+        `Incorrect. You answered ${candidateAnswers[i]}, but the correct answer is ${correctAnswers[i]}.`
+      );
     }
   }
 
-  
+  //TODO 3.2 use this variable to calculate the candidates score.
+  console.log(grade);
 
+  let percentCorrect = (grade.length / questions.length) * 100;
+  if (percentCorrect >= 80) {
+    console.log(
+      `Congratulations, ${candidateName}! You got ${percentCorrect}% of the answers right! You pass!`
+    );
+  } else {
+    console.log(
+      `You have failed the test, ${candidateName}-san. Only ${percentCorrect} were correct. You must now commit seppaku.`
+    );
+  }
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  // Compare the candidate answers with the correct answers,
+  // Calculate the candidate’s score as a percentage,
+  // (Number of Correct Answers) / (Number of Quiz Questions) * 100
+  // Convey to the candidate if they have passed the quiz with an 80% or if they have failed.
 
-
-  return grade;
+  return percentCorrect;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`Hello, ${candidateName} `);
+  console.log(`Hello, ${candidateName} `);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
@@ -84,5 +89,5 @@ module.exports = {
   correctAnswers: correctAnswers,
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
-  runProgram: runProgram
+  runProgram: runProgram,
 };
